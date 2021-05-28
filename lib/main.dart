ad7204e381504e4d5c14fbecd0fd42970f23a15a
babatundy <oussamaprobro@gmail.com>
2021-05-28 18:27:11 +0100

    just starting

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+Widget tag = Icon(Icons.account_circle);
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>  {
+  AnimationController animation;
+  var number = 0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    /*void go_new_route() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return Second_screen();
+          },
+        ),
+      );
+    }*/
+
+    return Scaffold(
+        backgroundColor: Colors.teal,
+        body: WebView(
+          initialUrl: "https://www.google.com/",
+          javascriptMode: JavascriptMode.unrestricted,
+        )
+    );
+  }
+}
