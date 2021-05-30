9924cc59f92010cd1a965d655599db91c7a95e74
babatundy <oussamaprobro@gmail.com>
2021-05-30 15:03:03 +0100

    Web view works fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
 void main() {
-  runApp(MaterialApp(home: Home(),));
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
 Widget tag = Icon(Icons.account_circle);
@@ -11,18 +14,53 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>  {
+class _HomeState extends State<Home> {
   AnimationController animation;
   var number = 0;
+  var text_controller = new TextEditingController();
+  String url;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.teal,
-        body: WebView(
-          initialUrl: "https://www.google.com",
-          javascriptMode: JavascriptMode.unrestricted,
-        )
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.tealAccent,
+          elevation: 60,
+          flexibleSpace: SafeArea(
+            child: Container(
+              child: TextField(
+                textAlign: TextAlign.center,
+                controller: text_controller,
+                onSubmitted: (text) {
+                  setState(() {
+                    url = text;
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return web(url);
+                        },
+                      ),
+                    );
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+        body: Container());
   }
+}
+
+Widget web(String url) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(url),
+    ),
+    body: WebView(
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
+    ),
+  );
 }
