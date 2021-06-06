67f785ef4ac715532c403d46fad2cbd1deb50f1a
babatundy <oussamaprobro@gmail.com>
2021-06-06 23:59:22 +0100

    fuck

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/platform_interface.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter_restart/flutter_restart.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,23 +21,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var connectivityResult = (Connectivity().checkConnectivity());
   AnimationController animation;
+  WebViewController controller;
+  //code -2  name net::ERR_INTERNET_DISCONNECTED
+  var web_error=WebResourceError(errorCode: -2, description: "net::ERR_INTERNET_DISCONNECTED");
   var number = 0;
   var text_controller = new TextEditingController();
-  String url="https://www.google.com";
+  String url = "https://www.google.com";
+  bool failed_to_build = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: Scaffold(
-        appBar: AppBar(
-          title: Text(url),
-        ),
-        body: WebView(
+      body: SafeArea(
+        child: failed_to_build==false? WebView(
+          onPageFinished: (s) {
+            setState(() {
+              failed_to_build = false;
+            });
+          },
+          onWebResourceError: (WebResourceError webResourceError) {
+            setState(() {
+
+              failed_to_build = true;
+            });
+          },
           initialUrl: url,
           javascriptMode: JavascriptMode.unrestricted,
-        ),
+          onWebViewCreated: (WebViewController wc) {
+            controller = wc;
+          },
+        ):RaisedButton(onPressed: (){
+          setState(() {
+            controller.reload();
+          });
+
+        },child: Text("ZABIIIIIIIIIIIIIIIIIIIIIIIIIIIII"),)
       ),
     );
   }
